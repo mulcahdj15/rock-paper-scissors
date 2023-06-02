4794ba8493b8a077a6928a7d69c8922fae14b68f
mulcahdj15 <dillon.mulcahy@gmail.com>
2023-06-02 21:04:25 +0200

    Creating the getComputerChoice() Function

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+// Computer's choice function
+function getComputerChoice(){
+
+// randomly generate a number and find its remainder when devided by 3
+var choice = (Math.random()*100) % 3;
+console.log(choice)
+// check this choice
+
+// if it is 0 return Rock
+
+// if it is 1 return Paper
+
+// if it is 2 or anything else return scissors
+
+}
