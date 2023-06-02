f6729186325b75522a6805d42b4b60ab243c05f2
mulcahdj15 <dillon.mulcahy@gmail.com>
2023-06-02 21:30:17 +0200

    Finished the computer choice function

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 // Computer's choice function
 function getComputerChoice(){
 
-// randomly generate a number and find its remainder when devided by 3
-var choice = (Math.random()*100) % 3;
-console.log(choice)
-// check this choice
+// randomly generate a number and find its remainder when divided by 3
+var choice = Math.floor((Math.random()*100) % 3);
 
-// if it is 0 return Rock
+switch (choice){ // check this choice
+ 
+    case 0: // if it is 0 return Rock
+        return "Rock";
+        break;
+    
+    case 1: // if it is 1 return Paper
+        return "Paper";
+        break;
 
-// if it is 1 return Paper
-
-// if it is 2 or anything else return scissors
-
+    case 2:// if it is 2 or anything else return scissors
+        return "Scissors";
+        break;    
 }
+}
+
+//define the function that asks for players selection
+
+
+
+//takes in he players input prompting them to make a choice
+
+//put all the players choices to lowercase
+
+//check if the selection is a rock paper or scissors selection. 
+
+// if not return a error message. 
+
+
+//close the function
